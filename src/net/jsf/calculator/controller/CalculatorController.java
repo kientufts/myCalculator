d33bb458a9a5ec332d0dec8893fb5114b45c9300
Kientufts <letrungkien1991@gmail.com>
2018-06-19 18:19:32 -0700

    finish basic features

--- src/net/jsf/calculator/controller/CalculatorController.java
+++ src/net/jsf/calculator/controller/CalculatorController.java
@@ -8,10 +8,35 @@ import javax.faces.bean.SessionScoped;
 @SessionScoped
 public class CalculatorController {
     public String display;
+    private int tmp;
+    private String operation;
 
     @PostConstruct
     public void init(){
         display="";
+    }
+
+    public void addOperation(String operation){
+        tmp = Integer.parseInt(display);
+        this.operation = operation;
+        display="";
+    }
+
+    public void calculate(){
+        int value = Integer.parseInt(display);
+        if("+".equals(operation)){
+            display = (tmp + value) + "";
+        } else if("-".equals(operation)) {
+            display = (tmp - value) + "";
+        } else if("*".equals(operation)) {
+            display = (tmp * value) + "";
+        } else if("/".equals(operation)) {
+            display = (tmp / value) + "";
+        }
+    }
+
+    public void clear(){
+        display = display.substring(0, display.length()-1);
     }
 
     public void addDigit(String digit){
